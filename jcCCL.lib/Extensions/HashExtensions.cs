e97e7e473d008b89dfa8baeca480979ecf36e30f
Jarred Capellman <jcapellman@hotmail.com>
2019-02-11 21:47:01 -0600

    Added: ComputeMD5 extension method

---
+++ jcCCL.lib/Extensions/HashExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace jcCCL.lib.Extensions
+{
+    public static class HashExtensions
+    {
+        public static string ComputeMD5(this byte[] data) => BitConverter.ToString(MD5.Create().ComputeHash(data));
+    }
+}
